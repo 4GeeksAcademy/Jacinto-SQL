5535dfae8a18160ef0713c802115a872528cfcb6
Jacinto-V <jacinto.velasco.sanz@outlook.es>
2025-05-09 10:38:02 +0000

    Completed exercises

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -8,32 +8,58 @@ SELECT * FROM climate;
 SELECT * FROM observations;
 
 
--- MISSION 1
--- Your query here
-
--- MISSION 2
--- Your query here:
-
-
--- MISSION 3
--- Your query here:
+SELECT regions.name, regions.country, COUNT(DISTINCT species.id) AS num_especies
+FROM observations
+JOIN species ON observations.species_id
+JOIN regions ON observations.region_id
+GROUP BY regions.name, regions.country
+ORDER BY num_especies DESC;
 
 
--- MISSION 4
--- Your query here:
+SELECT strftime('%m', observations.observation_date) AS mes, COUNT(*) AS num_observaciones
+FROM observations
+GROUP BY mes
+ORDER BY num_observaciones DESC;
 
 
--- MISSION 5
--- Your query here:
+SELECT species.scientific_name, species.common_name, COUNT(*) AS num_registros
+FROM observations
+JOIN species ON observations.species_id = species.id
+GROUP BY species_id
+HAVING num_registros < 10
+ORDER BY num_registros ASC;
 
 
--- MISSION 6
--- Your query here:
+SELECT regions.name, regions.country, COUNT(DISTINCT observations.species_id) AS especies_diferentes
+FROM observations
+JOIN regions ON observations.region_id = regions.id
+GROUP BY region.name, regions.country
+ORDER BY especies_diferentes DESC
+LIMIT 1;
 
 
--- MISSION 7
--- Your query here:
+SELECT species.scientific_name, species.common_name, COUNT(*) AS num_observaciones
+FROM observations
+JOIN species ON observations.species_id = species.id
+GROUP BY species.scientific_name, species.common_name
+ORDER BY num_observaciones DESC
+LIMIT 5;
 
 
--- MISSION 8
--- Your query here:
+SELECT observations.observer, COUNT(*) AS num_registros
+FROM observations
+GROUP BY observations.observer
+ORDER BY num_registros DESC
+LIMIT 10;
+
+
+SELECT species.scientific_name, species.common_name
+FROM species
+WHERE species.scientific_name NOT IN (SELECT DISTINCT species.scientific_name FROM observations);
+
+
+SELECT observations.observation_date, COUNT(DISTINCT observations.species_id) AS especies_observadas
+FROM observations
+GROUP BY observations.observation_date
+ORDER BY especies_observadas DESC
+LIMIT 5;
